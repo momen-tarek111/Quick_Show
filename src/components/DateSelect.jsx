7aebd1590a98fd74a47062d6e856d2b4834902e8
momen-tarek111 <momen.tarek.nagaty@gmail.com>
2026-09-08 03:59:09 +0300

    Update Movie Details UI

--- src/components/DateSelect.jsx
+++ src/components/DateSelect.jsx
@@ -17,7 +17,7 @@ function DateSelect({dateTime,id}) {
     <div id="dateSelect" className="pt-20">
         <div className="flex flex-col md:flex-row items-center justify-between gap-10 relative p-8 bg-primary/10 border border-primary/20 rounded-lg">
             <BlurCircle top="-100px" left="-100px"/>
-            <BlurCircle top="100px" right="0px"/>
+            <BlurCircle top="100px" right="-86px" />
             <div>
                 <p className="text-lg font-semibold">Choose Date</p>
                 <div className="flex items-center gap-6 text-sm mt-5">
